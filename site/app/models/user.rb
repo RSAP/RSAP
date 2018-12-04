d713a2135b16b60f6dee51f811bc92088ecce126
Carlos Antônio <carlosaajunior.jp@gmail.com>
2018-12-04 01:59:41 -0300

    Modelo amizade e relacionamento entre user e amizade

--- site/app/models/user.rb
+++ site/app/models/user.rb
@@ -1,8 +1,41 @@
 class User < ApplicationRecord
+
+	#Relacionamento com amizades
+	has_many :friendships
+	has_many :received_friendships, class_name: "Friendship", foreign_key: "friend_id"
+
+	has_many :active_friends, -> { where(friendships: { accepted: true}) }, through: :friendships, source: :friend
+	has_many :received_friends, -> { where(friendships: { accepted: true}) }, through: :received_friendships, source: :user
+	has_many :pending_friends, -> { where(friendships: { accepted: false}) }, through: :friendships, source: :friend
+	has_many :requested_friendships, -> { where(friendships: { accepted: false}) }, through: :received_friendships, source: :user
+
+
+	def friends
+		active_friends | received_friends
+	end
+
+
+	def pending
+		pending_friends | requested_friendships
+	end
+	############################################################################################3
+
+	def self.all_except(user)
+  		where.not(id: user)
+	end
+
+	def self.search(search)
+   		if search
+	 		where(['nome LIKE ?', "%#{search}%"])
+   		else
+	 		all
+   	end
+end
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
 
    #=============================
    has_attached_file :avatar, styles: {
@@ -13,6 +46,7 @@ class User < ApplicationRecord
 
    #Upload de apenas imagens (do tipo image)
    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+
 
 
    # Validate content type
