5421c87f22639f3d64fe2a10e575e5d6c01382d8
Carlos Antônio <carlosaajunior.jp@gmail.com>
2018-12-04 02:00:59 -0300

    Controlador de amizades (Ainda sendo refinado)

---
+++ site/app/controllers/friendships_controller.rb
@@ -0,0 +1,33 @@
+class FriendshipsController < ApplicationController
+	def create
+		@user = User.find(params[:friend_id])
+		if  (((current_user.pending_friends).include? @user) || ((current_user.requested_friendships).include? @user))
+			redirect_to controller: 'user', action: 'list', id: current_user.id, notice: "Ocorreu um erro ao tentar fazer a solicitação de amizade"
+		else
+			@friendship = current_user.friendships.build(friend_id: params[:friend_id])
+			if @friendship.save
+				flash[:notice] = "Pedido de amizade eviado."
+				puts @friendship
+			else
+				redirect_to controller: 'user', action: 'list', id: current_user.id, notice: "Ocorreu um erro ao tentar fazer a solicitação de amizade"
+			end
+		end
+	end
+
+	def update
+		@friendship = Friendship.find params[:request_id]
+		@friendship.update(accepted: true)
+		if @friendship.save
+			redirect_to controller: 'user', action: 'list', id: current_user.id, notice: "Voces agora são amigos!"
+		else
+			redirect_to controller: 'user', action: 'list', id: current_user.id, notice: "Ocorreu um erro ao tentar aceitar a solicitação de amizade"
+		end
+	end
+
+	def destroy
+		@friendship = Friendship.find_by(id: params[:request_id])
+		@friendship.destroy
+		flash[:notice] = "Amizade desfeita."
+		redirect_to controller: 'user', action: 'show', id: current_user.id
+	end
+end
