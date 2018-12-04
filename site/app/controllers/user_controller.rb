3156407db2a244dab5047024c6e4e1e48aeec551
Carlos Antônio <carlosaajunior.jp@gmail.com>
2018-12-04 02:01:18 -0300

    Adicao de alguns metodos ao controlador de usuario

--- site/app/controllers/user_controller.rb
+++ site/app/controllers/user_controller.rb
@@ -1,13 +1,32 @@
 class UserController < ApplicationController
 
+	def index
+		@user = User.search(params[:search])
+	end
+
    def create
       @user = User.create(user_params)
+   end
+
+   def viewUser
+	   @user = User.find(params[:idUser])
+	   @pendente = Friendship.all
    end
 
    #Para view de ver perfil
    def show
       @user = User.find(params[:id])
    end
+
+	def list
+		@user = User.find(params[:id])
+		@pendente = Friendship.all
+	end
+
+	def listFriends
+		@user = User.find(params[:id])
+	end
+
 
    private
 
