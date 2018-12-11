a5bb8955507d9f5bced00d33e95d591d5517881c
MBaltz <mateusbaltazar9@gmail.com>
2018-12-11 01:09:49 -0300

    Remoção de qualquer mensagem de erro repetida (voltadas a validações)

--- site/app/helpers/devise_helper.rb
+++ site/app/helpers/devise_helper.rb
@@ -1,28 +1,48 @@
 module DeviseHelper
-  def devise_error_messages!
-    return "" unless devise_error_messages?
+	def devise_error_messages!
+		return "" unless devise_error_messages?
 
-    messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
-    sentence = I18n.t("errors.messages.not_saved",
-                      :count => resource.errors.count,
-                      :resource => resource.class.model_name.human.downcase)
+		messages = resource.errors.full_messages.map { |msg| content_tag(:li, msg) }.join
+		sentence = I18n.t("errors.messages.not_saved",
+			:count => resource.errors.count,
+			:resource => resource.class.model_name.human.downcase)
 
-   #error_messages_for 'model', :header_message => "You have some errors that prevented saving this model"
+			#error_messages_for 'model', :header_message => "You have some errors that prevented saving this model"
 
-   # TODO: fazer for para retirar os duplicados
+			# TODO: fazer for para retirar os duplicados
 
-    html = <<-HTML
-    <div id="error_explanation">
-      <h2>#{sentence}</h2>
-      <ul>#{messages}</ul>
-    </div>
-    HTML
+			messages = removerRepetidas(messages)
 
-    html.html_safe
-  end
+			html = <<-HTML
+			<div id="error_explanation">
+				<h2>#{sentence}</h2>
+				<ul>#{messages}</ul>
+			</div>
+			HTML
 
-  def devise_error_messages?
-    !resource.errors.empty?
-  end
+			html.html_safe
+		end
 
-end
+		def devise_error_messages?
+			!resource.errors.empty?
+		end
+
+		def removerRepetidas(msgOriginal)
+			#dividir num vetor
+			vetorA = msgOriginal.split('</li>')
+			#concatena com oq ue foi removido na divisao
+			vetorA.map { |e| e.to_s << "</li>"  }
+			#remove os repetidos
+			vetorA = vetorA.uniq
+
+			#Junta tudo em uma string novamente
+			final = ""
+			for i in vetorA
+				final << i
+			end
+
+			#retorna uma string
+			return final
+		end
+
+	end
