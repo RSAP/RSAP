d0e9e5fc073ee259e91015e173b75d870541c629
Carlos Antônio <carlosaajunior.jp@gmail.com>
2018-12-18 14:23:22 -0300

    Refatoração? Do controlador de usuario

--- site/app/controllers/user_controller.rb
+++ site/app/controllers/user_controller.rb
@@ -19,35 +19,33 @@ class UserController < ApplicationController
 
 	#Para view de ver perfil
 	def show
-		@user = User.find(params[:id])
+		@user = buscarUsuario(params[:id])
+	end
+
+	def buscarUsuario id
+		User.find(id)
 	end
 
 	def home
-		@user = User.find(current_user.id)
+		@user = buscarUsuario(current_user.id)
 		@posts =Post.where(user_id: current_user.id)
 	end
 
 	def mural
-		@user = User.find(params[:id])
 		@posts = Post.where(user_id: params[:id])
 	end
+
 	def index
 		@user = User.search(params[:search])
 	end
 
 	def viewUser
-		@user = User.find(params[:idUser])
 		@pendente = Friendship.all
 	end
 
 
 	def list
-		@user = User.find(params[:id])
 		@pendente = Friendship.all
-	end
-
-	def listFriends
-		@user = User.find(params[:id])
 	end
 
 
