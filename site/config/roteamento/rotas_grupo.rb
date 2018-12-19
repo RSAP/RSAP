86e8a43238985400f554446d993410866f663c4c
Carlos Antônio <carlosaajunior.jp@gmail.com>
2018-12-19 18:09:50 -0300

    Views de grupo feitas (listar grupos, vizualizar grupo/membros)

--- site/config/roteamento/rotas_grupo.rb
+++ site/config/roteamento/rotas_grupo.rb
@@ -1,3 +1,8 @@
+
+#CARLOS
+match '/grupos/:id/membros' => 'grupo#verMembros', :as => :membros_grupo, via: :get
+
+#------------------------------------------------------------------------------------------------------------
 match 'grupos/' => 'grupo#index', via: :get
 match 'grupos/new/' => 'grupo#new', via: :get
 match 'grupos/:id/edit/' => 'grupo#edit', via: :get
